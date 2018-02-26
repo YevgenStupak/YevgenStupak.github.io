92461b89e01b88fe921c64b06e2ec6c8df48b16e
Yevgen Stupak <yevgenstupak@gmail.com>
2018-02-26 09:36:34 +0200

    !!!

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,8 @@
 //Slider Slick activate------//
 //top slider//
+
+
+
 $(document).ready(function(){
     $('.bannerr-slider').slick({
         appendArrows : '.banner-slider-button',
@@ -16,11 +19,62 @@ $(document).ready(function(){
     });
 });
 
-
-
-//menu scroll//
 $(document).ready(function(){
-    $("#menu").on("click","a",function(event){
+    var show = true;
+    var countbox = "#counts";
+    $(window).on("scroll load resize", function(){
+ 
+        if(!show) return false;                   // Отменяем показ анимации, если она уже была выполнена
+ 
+        var w_top = $(window).scrollTop();        // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top;     // Расстояние от блока со счетчиками до верха всего документа
+ 
+        var w_height = $(window).height();        // Высота окна браузера
+        var d_height = $(document).height();      // Высота всего документа
+ 
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+ 
+        if(w_top + 200 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height){
+            $("#spincr1").spincrement({
+                from: 0,
+                to:3891,
+                thousandSeparator: "",
+                duration: 3500
+            });
+            $("#spincr2").spincrement({
+                from: 0,
+                to:2810,
+                thousandSeparator: "",
+                duration: 3500
+            });
+            $("#spincr3").spincrement({
+                from: 0,
+                to:618,
+                thousandSeparator: "",
+                duration: 3500
+            });
+            $("#spincr4").spincrement({
+                from: 0,
+                to:178,
+                thousandSeparator: "",
+                duration: 3500
+            });
+            $("#spincr5").spincrement({
+                from: 0,
+                to:285,
+                thousandSeparator: "",
+                duration: 3500
+            });
+ 
+            show = false;
+        }
+    });
+});
+
+
+// menu scroll//
+$(document).ready(function(){
+    $("#menu,   #main-btn").on("click","a",function(event){
         event.preventDefault();
         var id = $(this).attr('href'),
             top = $(id).offset().top;
@@ -30,27 +84,76 @@ $(document).ready(function(){
 
 
 
-// button back-up//
-(function($) {  
-$(function() {  
-    
-    $('#back-top').click(function() {  
-    $('body,html').animate({scrollTop:0},2500);  
-    return false;  
-    });  
-    
-});  
-})(jQuery);
+// jQuery(document).ready(function ($) {
 
+// var lastId,
+// topMenu = $("#menu"),
+// topMenuHeight = topMenu.outerHeight(),
+//     // All list items
+//     menuItems = topMenu.find("a"),
+//     // Anchors corresponding to menu items
+//     scrollItems = menuItems.map(function () {
+//         var href = $(this).attr("href");
+//         if(href.indexOf("#") === 0){
+//             var item = $($(this).attr("href"));
+//             if (item.length) {
+//                 return item;
+//             }
+//         }
+//     });
+// });
+
+
+// function up() {  
+//     var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);  
+//   if(top > 0) {  
+//     window.scrollBy(0,((top+200)/-10));  
+//     t = setTimeout('up()',50);  
+//   } else clearTimeout(t);  
+//   return false;  
+//   }
+
+// button back-up//
+// (function($) {  
+// $(function() {  
+    
+//     $('#back-top').click(function() {  
+//     $('body,html').animate({scrollTop:0},2500);  
+//     return false;  
+//     });  
+    
+// });  
+// })(jQuery);
+
+
+jQuery(document).ready(function (){
+
+    $(window).scroll(function () {
+        
+        //Display or hide scroll to top button 
+        if  ($(this).scrollTop()>500) {
+            $('#back-top').fadeIn(1000);
+        } else {
+            $('#back-top').fadeOut(600);
+        }
+    });
+    $('#back-top').click(function () {
+        $("html,body").animate({
+            scrollTop: 0
+            }, 800);
+            return false;
+        
+    });
+});
 
 // wow effekt//
-newFunction();
+
 
 function newFunction() {
     var wow = new WOW({
         boxClass: 'wow',
         animateClass: 'animated',
-        offset: 200,
+        offset: 300,
         mobile: false,
         live: true,
         callback: function(box) {
@@ -61,10 +164,11 @@ function newFunction() {
     });
     wow.init();
 }
-newFunction_1();
+newFunction();
 
 
 // header-resize//
+
 function newFunction_1() {
     $(window).scroll(function() {
         if ($(this).scrollTop() > 50) {
@@ -85,9 +189,77 @@ $(document).ready(function(){
         men.slideToggle();
     });
     $(window).resize(function(){
+
         var wid = $(window).width();
+
         if(wid > 760 && men.is(':hidden')) {
             men.removeAttr('style');
         }
     });
 });
+newFunction_1();
+
+
+
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            $('.filter').show(500);
+        }
+        else
+        {
+            $(".filter").not('.'+value).hide(500);
+            $('.filter').filter('.'+value).show(500);
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+
+$(document).ready(function() {
+    
+    //E-mail Ajax Send
+    $("#reg").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "../form/submit.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            
+            // $('.hidden').fadeIn(500);
+
+            setTimeout(function() {
+                $('.hidden, .close').click(function() {
+                    $('.hidden').fadeOut(500);
+                });
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+
+});
+
+ $(document).ready(function(){
+    
+     $('.form-btn').click(function() {
+       
+         $('.hidden').fadeIn(500);
+     });
+
+     $('.hidden, .close').click(function() {
+
+         $('.hidden').fadeOut(500);
+     });
+ });
